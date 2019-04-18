b0f18de72ffb9b048c2c7a3d762685059176414f
Wilbert Almodovar <billywag@gmail.com>
2019-04-18 17:55:30 -0500

    wrong use of parameters

--- lib/main.dart
+++ lib/main.dart
@@ -37,11 +37,11 @@ class _PartsSalePageState extends State<PartsSalePage> {
     var time = new DateFormat.jm().format(new DateTime.now().toUtc());
   //  var makeId = "1";
     var partListRes =
-    await http.post(Uri.encodeFull(listUrl), body: { "makeId":_makesSelection, "modelId":_modelSelection, "yearFrom":_fromYear, "yearTo":_toYear, "partId":_partsSelection}, headers: {
+    await http.post(Uri.encodeFull(listUrl), headers: {
       "content-type": "application/json",
       "token": base64.encode(
           utf8.encode("9cec20bea9c34668bc443b67a3e99e23|" + date + " " + time))
-    });
+    }, body: { "makeId":_makesSelection, "modelId":_modelSelection, "yearFrom":_fromYear, "yearTo":_toYear, "partId":_partsSelection});
     var partListResBody = json.decode(partListRes.body);
 
     if (partListResBody['success'] == true) {
